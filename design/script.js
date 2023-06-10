7f687e2c73be56c7ec6ff9ae8fa3633a19ec4cf8
Surakat Samuel <Surakat.s@bullseye.local>
2023-06-10 12:18:07 +0100

    Progress

---
+++ design/script.js
@@ -0,0 +1,10 @@
+
+let current_ip = document.getElementById('current_ip')
+let current_town = document.getElementById('current_town')
+let current_zone = document.getElementById('current_zone')
+let current_isp = document.getElementById('current_isp')
+
+// form elements 
+const entered_ip = document.getElementById('ip_address') 
+const search_btn = document.getElementById('search_btn')
+
