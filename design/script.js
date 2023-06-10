f673abe65c2960632fe4a2293b16b4d6e9d020ac
Surakat Samuel <Surakat.s@bullseye.local>
2023-06-10 12:30:43 +0100

    Still in progress

--- design/script.js
+++ design/script.js
@@ -4,7 +4,36 @@ let current_town = document.getElementById('current_town')
 let current_zone = document.getElementById('current_zone')
 let current_isp = document.getElementById('current_isp')
 
-// form elements 
-const entered_ip = document.getElementById('ip_address') 
+
+const entered_ip = document.getElementById('Search_bar') 
 const search_btn = document.getElementById('search_btn')
+
+function fetchLocation() {
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+        const ipAddress = data.ip;
+  
+        fetch('http://api.ipstack.com/' + ipAddress + '?access_key=YOUR_API_KEY')
+          .then(response => response.json())
+          .then(data => {
+            const latitude = data.latitude;
+            const longitude = data.longitude;
+  
+            
+            console.log('Latitude:', latitude);
+            console.log('Longitude:', longitude);
+          })
+          .catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
+  }
+  
+  
+  fetchLocation();
+  
+
+
+
+
 
