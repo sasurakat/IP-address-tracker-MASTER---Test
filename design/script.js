3668deba2e691c4fd8609bf897d1c85b4a8736c7
Surakat Samuel <Surakat.s@bullseye.local>
2023-06-10 14:40:53 +0100

    Progress

--- design/script.js
+++ design/script.js
@@ -1,28 +1,61 @@
+let current_ip = document.getElementById('currentip');
+let current_town = document.getElementById('currenttown');
+let current_zone = document.getElementById('currentzone');
+let current_isp = document.getElementById('currentisp');
 
-let current_ip = document.getElementById('current_ip')
-let current_town = document.getElementById('current_town')
-let current_zone = document.getElementById('current_zone')
-let current_isp = document.getElementById('current_isp')
+const entered_ip = document.querySelector('.Search_bar');
+const search_btn = document.querySelector('.search_btn');
 
+const api_key = 'YOUR_API_KEY'; // Replace with your actual API key
 
-const entered_ip = document.getElementById('Search_bar') 
-const search_btn = document.getElementById('search_btn')
+const map = L.map('MAPDISPLAY', {
+  center: [0, 0],
+  zoom: 0,
+  layers: [
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    })
+  ]
+});
 
-function fetchLocation() {
-    fetch('https://api.ipify.org?format=json')
-      .then(response => response.json())
-      .then(data => {
-        const ipAddress = data.ip;
-  
-      })
-      .catch(error => console.log(error));
+const updateMarker = (update_marker = [-33.665, 18.993]) => {
+  map.setView(update_marker, 13);
+  L.marker(update_marker).addTo(map);
+};
+
+const getIPDetails = (default_ip) => {
+  let ip_url;
+  if (default_ip === undefined) {
+    ip_url = `https://api.ipapi.com/check?access_key=${api_key}`;
+  } else {
+    ip_url = `https://api.ipapi.com/${default_ip}?access_key=${api_key}`;
   }
-  
-  
-  fetchLocation();
-  
+  fetch(ip_url)
+    .then(response => response.json())
+    .then(data => {
+      current_ip.innerHTML = data.ip;
+      current_town.innerHTML = `${data.city}, ${data.region}, ${data.country} ${data.postal}`;
+      current_zone.innerHTML = data.timezone;
+      current_isp.innerHTML = data.isp;
 
+      // update map marker
+      updateMarker([data.latitude, data.longitude]);
+    })
+    .catch(error => {
+      alert("Unable to get IP details");
+      console.log(error);
+    });
+};
 
+document.addEventListener('DOMContentLoaded', () => {
+  updateMarker();
+});
 
-
-
+search_btn.addEventListener('click', e => {
+  e.preventDefault();
+  if (entered_ip.value.trim() !== '') {
+    getIPDetails(entered_ip.value);
+  } else {
+    alert("Please enter a valid IP address");
+  }
+});
