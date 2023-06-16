35d151aad6c738b6fc9f16748dd4e190b48be143
Surakat Samuel <Surakat.s@bullseye.local>
2023-06-16 22:02:33 +0100

    Updated

--- design/script.js
+++ design/script.js
@@ -1,61 +1,68 @@
-let current_ip = document.getElementById('currentip');
-let current_town = document.getElementById('currenttown');
-let current_zone = document.getElementById('currentzone');
-let current_isp = document.getElementById('currentisp');
+'use strict';
 
-const entered_ip = document.querySelector('.Search_bar');
-const search_btn = document.querySelector('.search_btn');
+// Import API_KEY from key.js
+const API_KEY = '6f3816355e174553ab98cf5740385a0a';
 
-const api_key = 'YOUR_API_KEY'; // Replace with your actual API key
+// Get HTML elements
+var current_ip = document.getElementById('currentip');
+var current_town = document.getElementById('currenttown');
+var current_zone = document.getElementById('current_zone');
+var current_isp = document.getElementById('currentisp');
 
-const map = L.map('MAPDISPLAY', {
-  center: [0, 0],
-  zoom: 0,
-  layers: [
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    })
-  ]
-});
+var entered_ip = document.querySelector('.input-field');
+var search_btn = document.querySelector('.submit-button');
 
-const updateMarker = (update_marker = [-33.665, 18.993]) => {
+// Create Leaflet map
+var map = L.map('MAPDISPLAY').setView([34.0614, -118.08162], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// Function to update map marker and view
+var updateMarker = (update_marker = [9.0765, 7.3986]) => {
   map.setView(update_marker, 13);
   L.marker(update_marker).addTo(map);
 };
 
-const getIPDetails = (default_ip) => {
+// Function to get IP details
+var getIPDetails = (default_ip) => {
   let ip_url;
   if (default_ip === undefined) {
-    ip_url = `https://api.ipapi.com/check?access_key=${api_key}`;
+    ip_url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=`;
   } else {
-    ip_url = `https://api.ipapi.com/${default_ip}?access_key=${api_key}`;
+    ip_url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=${default_ip}`;
   }
   fetch(ip_url)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       current_ip.innerHTML = data.ip;
-      current_town.innerHTML = `${data.city}, ${data.region}, ${data.country} ${data.postal}`;
-      current_zone.innerHTML = data.timezone;
+      current_town.innerHTML = `${data.city}, ${data.country_name}, ${data.postal_code}`;
+      current_zone.innerHTML = `UTC${data.time_zone.offset}`;
       current_isp.innerHTML = data.isp;
 
-      // update map marker
+      // Update map marker
       updateMarker([data.latitude, data.longitude]);
     })
-    .catch(error => {
-      alert("Unable to get IP details");
+    .catch((error) => {
+      alert('Unable to get IP details');
       console.log(error);
     });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  updateMarker();
-});
+// Event listener when the DOM is loaded
+document.addEventListener('DOMContentLoaded', updateMarker);
 
-search_btn.addEventListener('click', e => {
+// Event listener for search button click
+search_btn.addEventListener('click', (e) => {
   e.preventDefault();
   if (entered_ip.value.trim() !== '') {
     getIPDetails(entered_ip.value);
   } else {
-    alert("Please enter a valid IP address");
+    alert('Please enter a valid IP address');
   }
 });
+
+
+
+
